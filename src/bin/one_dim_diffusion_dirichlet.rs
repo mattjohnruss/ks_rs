0a65d7ce5382f39e2533797c156c0300bd6f5c27
Matthew Russell <matthewjohnrussell@gmail.com>
2019-09-18 20:58:51 +0100

    Update Dirichlet BCs test driver
    * Use Euler forward timestepping for now
    * Output cell averages in addition to piecewise linear reconstruction
    * Trace the values at the boundaries as calculated by both the regular
      method and the Dirichlet BC method

--- src/bin/one_dim_diffusion_dirichlet.rs
+++ src/bin/one_dim_diffusion_dirichlet.rs
@@ -9,8 +9,8 @@ use ks_rs::adr::one_dim::{
 };
 use ks_rs::timestepping::{
     ExplicitTimeStepper,
-    //EulerForward,
-    SspRungeKutta33,
+    EulerForward,
+    //SspRungeKutta33,
 };
 use std::fs;
 use std::io::{Write, BufWriter};
@@ -26,12 +26,16 @@ impl DiffusionDirichlet {
 }
 
 impl ProblemFunctions for DiffusionDirichlet {
+    fn velocity_p_at_midpoint(&self, _problem: &Problem1D<Self>, _var: Variable, _cell: Cell) -> f64 {
+        1.0
+    }
+
     fn left_bc(&self, _problem: &Problem1D<Self>, _var: Variable) -> BoundaryCondition {
-        BoundaryCondition::Dirichlet(1.0)
+        BoundaryCondition::Dirichlet(0.0)
     }
 
     fn right_bc(&self, _problem: &Problem1D<Self>, _var: Variable) -> BoundaryCondition {
-        BoundaryCondition::Dirichlet(0.0)
+        BoundaryCondition::Dirichlet(1.0)
     }
 }
 
@@ -41,27 +45,28 @@ fn set_initial_conditions<F>(problem: &mut Problem1D<F>)
     //*problem.var_mut(DiffusionDirichlet::C, Cell(1)) = 1.0;
 
     for cell in problem.interior_cells() {
-        *problem.var_mut(DiffusionDirichlet::C, cell) = (1.0 - problem.x(cell)).powi(2);
+        *problem.var_mut(DiffusionDirichlet::C, cell) = 1.0 - (1.0 - problem.x(cell)).powi(2);
     }
 }
 
 fn trace_header<W: Write>(buffer: &mut W) -> std::io::Result<()> {
-    buffer.write_all(b"t c_left c_right\n")
+    buffer.write_all(b"t c_left_d c_right_d c_left c_right\n")
 }
 
 fn trace<F, W>(problem: &Problem1D<F>, buffer: &mut W) -> std::io::Result<()>
     where F: ProblemFunctions,
           W: Write
 {
+    let left_val_d = problem.var_point_value_at_face_for_dirichlet_bcs(DiffusionDirichlet::C, Cell(1), Face::West);
+    let right_val_d = problem.var_point_value_at_face_for_dirichlet_bcs(DiffusionDirichlet::C, Cell(problem.domain.n_cell), Face::East);
     let left_val = problem.var_point_value_at_face(DiffusionDirichlet::C, Cell(1), Face::West);
     let right_val = problem.var_point_value_at_face(DiffusionDirichlet::C, Cell(problem.domain.n_cell), Face::East);
-    buffer.write_all(format!("{} {} {}\n", problem.time, left_val, right_val).as_bytes())
+    buffer.write_all(format!("{} {} {} {} {}\n", problem.time, left_val_d, right_val_d, left_val, right_val).as_bytes())
 }
 
 fn main() -> Result<()> {
-    let domain = DomainParams { n_cell: 11, width: 1.0 };
+    let domain = DomainParams { n_cell: 101, width: 1.0 };
 
-    // Use the parameters from the Painter and Hillen paper
     let diffusion_dirichlet = DiffusionDirichlet {};
 
     let mut problem = Problem1D::new(DiffusionDirichlet::N_VARIABLE, domain, diffusion_dirichlet);
@@ -73,15 +78,18 @@ fn main() -> Result<()> {
     let dir_path = Path::new(&dir);
     fs::create_dir_all(dir_path)?;
 
-    //let mut euler_forward = EulerForward::new(problem.n_dof);
-    let mut ssp_rk33 = SspRungeKutta33::new(problem.n_dof);
+    let mut euler_forward = EulerForward::new(problem.n_dof);
+    //let mut ssp_rk33 = SspRungeKutta33::new(problem.n_dof);
 
     let file = fs::File::create(dir_path.join(format!("output_{:05}.csv", 0)))?;
+    let cell_averages_file = fs::File::create(dir_path.join(format!("output_averages_{:05}.csv", 0)))?;
     let mut buf_writer = BufWriter::new(file);
+    let mut cell_averages_buf_writer = BufWriter::new(cell_averages_file);
     problem.update_ghost_cells();
     problem.output(&mut buf_writer)?;
-    //problem.output_cell_averages(&mut buf_writer)?;
+    problem.output_cell_averages(&mut cell_averages_buf_writer)?;
     buf_writer.flush()?;
+    cell_averages_buf_writer.flush()?;
 
     let trace_file = fs::File::create(dir_path.join("trace.csv"))?;
     let mut trace_writer = BufWriter::new(trace_file);
@@ -94,15 +102,17 @@ fn main() -> Result<()> {
     let t_max = 1.0;
 
     while problem.time < t_max {
-        //euler_forward.step(&mut problem, dt);
-        ssp_rk33.step(&mut problem, dt);
+        euler_forward.step(&mut problem, dt);
+        //ssp_rk33.step(&mut problem, dt);
 
         if i % output_interval == 0 {
             let file = fs::File::create(dir_path.join(format!("output_{:05}.csv", i / output_interval)))?;
+            let cell_averages_file = fs::File::create(dir_path.join(format!("output_averages_{:05}.csv", i / output_interval)))?;
             let mut buf_writer = BufWriter::new(file);
+            let mut cell_averages_buf_writer = BufWriter::new(cell_averages_file);
             println!("Outputting at time = {}, i = {}", problem.time, i);
             problem.output(&mut buf_writer)?;
-            //problem.output_cell_averages(&mut buf_writer)?;
+            problem.output_cell_averages(&mut cell_averages_buf_writer)?;
             trace(&problem, &mut trace_writer)?;
         }
         i += 1;
