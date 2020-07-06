6f6b46d9d7b4c94cfa2f03ef277fa7d65bd40b1a
Matthew Russell <matthewjohnrussell@gmail.com>
2020-07-06 19:50:42 +0100

    add n_procs arg to point values script

--- scripts/all_point_values.sh
+++ scripts/all_point_values.sh
@@ -2,6 +2,7 @@
 
 n_files=1000
 n_cells=100
+n_procs=4
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -11,6 +12,9 @@ while [ "$1" != "" ]; do
         -c ) shift
             n_cells=$1
             ;;
+        -j ) shift
+            n_procs=$1
+            ;;
     esac
     shift
 done
@@ -18,4 +22,4 @@ done
 lines=$(seq -s " " 2 3 $((3 * $n_cells + 1)))
 
 # FIXME total hack to hardcode this...
-parallel -j4 --progress /home/pmzmr/Code/rust/numerics/ks_rs/scripts/point_values_vs_time.sh -f "$n_files" -i {} ::: $lines
+parallel -j"$n_procs" --progress /home/pmzmr/Code/rust/numerics/ks_rs/scripts/point_values_vs_time.sh -f "$n_files" -i {} ::: $lines
