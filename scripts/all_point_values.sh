d7398c6252e032ec32c78b64db25c0343f0b195a
Matthew Russell <matthewjohnrussell@gmail.com>
2020-09-25 19:38:10 +0100

    update path for servers

--- scripts/all_point_values.sh
+++ scripts/all_point_values.sh
@@ -22,4 +22,4 @@ done
 lines=$(seq -s " " 2 3 $((3 * $n_cells + 1)))
 
 # FIXME total hack to hardcode this...
-parallel -j"$n_procs" --progress /home/pmzmr/Code/rust/numerics/ks_rs/scripts/point_values_vs_time.sh -f "$n_files" -i {} ::: $lines
+parallel -j"$n_procs" --progress /home/pmzmr/ks_rs/scripts/point_values_vs_time.sh -f "$n_files" -i {} ::: $lines
