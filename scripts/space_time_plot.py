cd09398d8509e9b2cb4e1c322d13270c70c4777d
Matthew Russell <matthewjohnrussell@gmail.com>
2021-09-22 15:03:35 +0100

    Some improvements to space-time plots

--- scripts/space_time_plot.py
+++ scripts/space_time_plot.py
@@ -4,7 +4,7 @@ import matplotlib as mpl
 from glob import glob
 from sys import argv
 
-file_list = np.sort(glob("./*.csv"))
+file_list = np.sort(glob("./output_*.csv"))
 n_files = len(file_list)
 n_points = int(argv[1])
 n_var = int(argv[2])
@@ -12,19 +12,21 @@ n_timesteps = int(argv[3])
 dt = float(argv[4])
 
 # initialise empty storage
-data = np.zeros([n_files, n_points, n_var])
+data = np.zeros([n_files, n_points, n_var + 2])
 
 # loop over files and read data
 for (i, file_path) in enumerate(file_list):
-    data[i] = np.genfromtxt(file_path, skip_header=1, usecols=(2, 3, 4,
+    data[i] = np.genfromtxt(file_path, skip_header=1, usecols=(0, 1, 2, 3, 4,
                                                                5, 6, 7, 8))
 
+# extract the times and positions from the data, and then remove them
+t = data[:, 0, 0]
+x = data[0, :, 1]
 
-x = np.linspace(0, 1, n_points)
-t = np.arange(0, n_timesteps*dt, dt)
+# remove the times and positions from the data
+data = data[:, :, 2:]
 
 aspect_ratio = x.max()/t.max()
-# aspect_ratio = t.max()/x.max()
 
 mpl.rc('mathtext', fontset='cm')
 
@@ -42,13 +44,18 @@ for (i, var_name) in enumerate(var_names):
     ax.set_ylabel(r'$t$')
 
     # hack to reduce colobar range of phi_c_b
-    vmax = None
-    if i == 6:
-        vmax = 20
+    # vmax = None
+    # if i == 6:
+    #     vmax = 20
+
+    # im = ax.imshow(data[0:n_timesteps, :, i], interpolation='none',
+    #                origin='lower', extent=[x.min(), x.max(), t.min(),
+    #                t.max()],
+    #                vmax=vmax, aspect=aspect_ratio, cmap='inferno')
 
     im = ax.imshow(data[0:n_timesteps, :, i], interpolation='none',
                    origin='lower', extent=[x.min(), x.max(), t.min(), t.max()],
-                   vmax=vmax, aspect=aspect_ratio, cmap='inferno')
+                   aspect=aspect_ratio, cmap='inferno')
 
     # divider = make_axes_locatable(ax)
     # cax = divider.append_axes("right", size="5%", pad=0.01)
@@ -56,13 +63,13 @@ for (i, var_name) in enumerate(var_names):
 
     # hack to use 2 digits after decimal points, except for phi_c_b, to make
     # all the labels the same resulting length
-    format = '%.2f'
-    if i == 6:
-        format = '%.1f'
+    # format = '%.2f'
+    # if i == 6:
+    #     format = '%.1f'
 
-    plt.colorbar(im, format=format)
+    # plt.colorbar(im, format=format)
+    plt.colorbar(im)
 
     ax.set_title(var_name)
 
-    # plt.show()
     plt.savefig(f'{simple_var_names[i]}.pdf', bbox_inches='tight')
