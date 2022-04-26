bb4c4674e65e06daeea64d93b5669831c31a9738
Matthew Russell <matthewjohnrussell@gmail.com>
2022-04-26 17:29:48 +0100

    Remove old combined quantity gen from script

--- scripts/sensitivity/run_all.bash
+++ scripts/sensitivity/run_all.bash
@@ -10,27 +10,3 @@ n_proc="16"
 
 #echo "Running simulations:"
 #time parallel -j"$n_proc" ./scripts/sensitivity/run_one.bash {1} "$res_dir" ::: $(seq 1 $n_runs) > /dev/null
-
-# TODO we have yet to decide what quantities to extract from the simulations
-# for the sensitivity analysis (the simple code commented out below is from a
-# test problem), so for now don't do any output combining etc. We can do this
-# later after running the simulations. See comments in the R script for other
-# details.
-
-#echo "Combining simulation outputs:"
-#time for i in $(seq 0 $(expr $n_runs - 1))
-#do
-    #cat res/$i/output.csv
-#done > all_outputs.csv
-
-echo "Combining simulation outputs:"
-
-# Get header from first file
-awk 'NR==1' "$res_dir"/0/trace.csv > "$res_dir"/all_outputs.csv
-
-time for i in $(seq 0 $(expr $n_runs - 1))
-do
-    #cat res/$i/output.csv
-    # get trace file at t = 1000 (line 1002)
-    awk 'NR==302' "$res_dir"/$i/trace.csv
-done >> "$res_dir"/all_outputs.csv
