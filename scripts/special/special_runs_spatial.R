f48f3021ecb454d7c387e802e7b8f2c06152f81b
Matthew Russell <matthewjohnrussell@gmail.com>
2022-08-18 01:32:25 +0100

    Better spatial plots

--- scripts/special/special_runs_spatial.R
+++ scripts/special/special_runs_spatial.R
@@ -55,8 +55,8 @@ spatial_plot_subset <- function(time,
   )
 
   if (!missing(linetype_by)) {
-      p <- p + aes(linetype = factor({{ linetype_by }})) +
-        labs(linetype = param_labels[deparse(substitute(linetype_by))])
+    p <- p + aes(linetype = factor({{ linetype_by }})) +
+      labs(linetype = param_labels[deparse(substitute(linetype_by))])
   }
 
   if (!missing(alpha_by)) {
@@ -85,41 +85,199 @@ spatial_plot_subset <- function(time,
   p
 }
 
-p_spatial_homeostasis <- spatial_plot_subset(
-  0,
-  list(
-    j_phi_i_i_factor = 1000,
-    m_i_factor = 1000,
-    t_j_phi_i_lag = 0,
-    gamma = c(0, 1),
-    pe = c(-5, -3, -1, 1, 3, 5)
-  ),
-  colour_by = pe,
-  linetype_by = gamma
+spatial_plot_subset_2 <- function(time,
+                                  params_subset,
+                                  colour_by,
+                                  linetype_by,
+                                  alpha_by) {
+  data <- read_spatial_data(params, time, res_dir_base)
+
+  spatial_plot_panel <- function(variable) {
+    p <- ggplot(
+      data[
+        j_phi_i_i_factor %in% as.numeric(params_subset$j_phi_i_i_factor) &
+          m_i_factor %in% as.numeric(params_subset$m_i_factor) &
+          t_j_phi_i_lag %in% as.numeric(params_subset$t_j_phi_i_lag) &
+          gamma %in% as.numeric(params_subset$gamma) &
+          pe %in% as.numeric(params_subset$pe)
+      ],
+      aes(
+        x = x,
+        y = {{ variable }},
+        group = rep,
+        colour = factor({{ colour_by }})
+      )
+    )
+
+    p <- p +
+      geom_line(size = 1) +
+      theme(strip.background = element_blank(), strip.placement = "outside") +
+      labs(
+        x = expression(x),
+        y = variable_labels[deparse(substitute(variable))],
+        colour = param_labels[deparse(substitute(colour_by))]
+      )
+    p
+  }
+
+  plots <- list()
+
+  plots[[1]] <- spatial_plot_panel(C_b) +
+    labs(title = paste(
+        "Time since inflammation: ",
+        round(data[, time_inf] %>% unique, 3)
+      )
+    )
+  plots[[2]] <- spatial_plot_panel(phi_C_b)
+
+  if (!missing(linetype_by)) {
+    for (i in 1:length(plots)) {
+      plots[[i]] <- plots[[i]] + aes(linetype = factor({{ linetype_by }})) +
+        labs(linetype = param_labels[deparse(substitute(linetype_by))])
+    }
+  }
+
+  if (!missing(alpha_by)) {
+    # scale_alpha_manual only works for a parameter with at most the number of
+    # values specified here
+    for (i in 1:length(plots)) {
+      plots[[i]] <- plots[[i]] + aes(alpha = factor({{ alpha_by }})) +
+        scale_alpha_manual(values = c(1.0, 0.4)) +
+        labs(alpha = param_labels[deparse(substitute(alpha_by))])
+    }
+  }
+
+  plots
+}
+
+# j_phi_i_i_factor = 2, m_i_factor = 2
+
+params_subset <- list(
+  j_phi_i_i_factor = 2,
+  m_i_factor = 2,
+  t_j_phi_i_lag = 0,
+  gamma = c(0, 1),
+  pe = c(-5, -3, -1, 1, 3, 5)
 )
-p_spatial_homeostasis
+
+p_spatial_2_2 <- lapply(seq(0, 500, 100), function(t_inf) {
+    spatial_plot_subset_2(
+      t_inf,
+      params_subset,
+      colour_by = pe,
+      linetype_by = gamma
+    )
+})
+
+p_spatial_2_2 <- wrap_plots(
+  rlang::flatten(p_spatial_2_2),
+  ncol = 2,
+  guides = "collect"
+) +
+plot_annotation(tag_levels = "A")
 
 ggsave_with_defaults(
-  plot = p_spatial_homeostasis,
-  paste(plot_dir, "spatial_homeostasis.pdf", sep = "/")
+  plot = p_spatial_2_2,
+  paste(plot_dir, "spatial_2_2.pdf", sep = "/"),
+  width = 8.5,
+  height = 14
 )
 
-p_spatial_early <- spatial_plot_subset(
-  10,
-  list(
-    j_phi_i_i_factor = 1000,
-    m_i_factor = c(2, 1000),
-    t_j_phi_i_lag = 0,
-    gamma = c(0, 1),
-    pe = c(-5, -3, -1, 1, 3, 5)
-  ),
-  colour_by = pe,
-  linetype_by = gamma,
-  alpha_by = m_i_factor
+# j_phi_i_i_factor = 1000, m_i_factor = 2
+
+params_subset <- list(
+  j_phi_i_i_factor = 1000,
+  m_i_factor = 2,
+  t_j_phi_i_lag = 0,
+  gamma = c(0, 1),
+  pe = c(-5, -3, -1, 1, 3, 5)
 )
-p_spatial_early
+
+p_spatial_1000_2 <- lapply(seq(0, 500, 100), function(t_inf) {
+    spatial_plot_subset_2(
+      t_inf,
+      params_subset,
+      colour_by = pe,
+      linetype_by = gamma
+    )
+})
+
+p_spatial_1000_2 <- wrap_plots(
+  rlang::flatten(p_spatial_1000_2),
+  ncol = 2,
+  guides = "collect"
+) +
+plot_annotation(tag_levels = "A")
 
 ggsave_with_defaults(
-  plot = p_spatial_early,
-  paste(plot_dir, "spatial_early.pdf", sep = "/")
+  plot = p_spatial_1000_2,
+  paste(plot_dir, "spatial_1000_2.pdf", sep = "/"),
+  width = 8.5,
+  height = 14
+)
+
+# j_phi_i_i_factor = 2, m_i_factor = 1000
+
+params_subset <- list(
+  j_phi_i_i_factor = 2,
+  m_i_factor = 1000,
+  t_j_phi_i_lag = 0,
+  gamma = c(0, 1),
+  pe = c(-5, -3, -1, 1, 3, 5)
+)
+
+p_spatial_2_1000 <- lapply(seq(0, 500, 100), function(t_inf) {
+    spatial_plot_subset_2(
+      t_inf,
+      params_subset,
+      colour_by = pe,
+      linetype_by = gamma
+    )
+})
+
+p_spatial_2_1000 <- wrap_plots(
+  rlang::flatten(p_spatial_2_1000),
+  ncol = 2,
+  guides = "collect"
+) +
+plot_annotation(tag_levels = "A")
+
+ggsave_with_defaults(
+  plot = p_spatial_2_1000,
+  paste(plot_dir, "spatial_2_1000.pdf", sep = "/"),
+  width = 8.5,
+  height = 14
+)
+
+# j_phi_i_i_factor = 1000, m_i_factor = 1000
+
+params_subset <- list(
+  j_phi_i_i_factor = 1000,
+  m_i_factor = 1000,
+  t_j_phi_i_lag = 0,
+  gamma = c(0, 1),
+  pe = c(-5, -3, -1, 1, 3, 5)
+)
+
+p_spatial_1000_1000 <- lapply(seq(0, 500, 100), function(t_inf) {
+    spatial_plot_subset_2(
+      t_inf,
+      params_subset,
+      colour_by = pe,
+      linetype_by = gamma
+    )
+})
+
+p_spatial_1000_1000 <- wrap_plots(
+  rlang::flatten(p_spatial_1000_1000),
+  ncol = 2,
+  guides = "collect"
+) +
+plot_annotation(tag_levels = "A")
+
+ggsave_with_defaults(
+  plot = p_spatial_1000_1000,
+  paste(plot_dir, "spatial_1000_1000.pdf", sep = "/"),
+  width = 8.5,
+  height = 14
 )
