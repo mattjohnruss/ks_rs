c9d071202d5488412de5917005ea519ce557ac75
Matthew Russell <matthewjohnrussell@gmail.com>
2022-08-24 18:10:20 +0100

    Kill old plot functions

--- scripts/special/special_runs_spatial.R
+++ scripts/special/special_runs_spatial.R
@@ -30,135 +30,11 @@ params <- expand.grid(
 # pe on the RHS references the global variable)
 add_constants_and_gammas_to_param_table(params)
 
-spatial_plot_subset <- function(time,
+spatial_plot_subset <- function(times,
                                 params_subset,
                                 colour_by,
                                 linetype_by,
                                 alpha_by) {
-  data <- read_spatial_data(params, time, res_dir_base)
-  data_long <- melt(
-    data,
-    measure.vars = c("C_b", "phi_C_b")
-  )
-  p <- ggplot(
-    data_long[
-      j_phi_i_i_factor %in% as.numeric(params_subset$j_phi_i_i_factor) &
-        m_i_factor %in% as.numeric(params_subset$m_i_factor) &
-        t_j_phi_i_lag %in% as.numeric(params_subset$t_j_phi_i_lag) &
-        gamma %in% as.numeric(params_subset$gamma) &
-        pe %in% as.numeric(params_subset$pe)
-    ],
-    aes(
-      x = x,
-      y = value,
-      group = rep,
-      colour = factor({{ colour_by }})
-    )
-  )
-
-  if (!missing(linetype_by)) {
-    p <- p + aes(linetype = factor({{ linetype_by }})) +
-      labs(linetype = param_labels[deparse(substitute(linetype_by))])
-  }
-
-  if (!missing(alpha_by)) {
-    # scale_alpha_manual only works for a parameter with at most the number of
-    # values specified here
-      p <- p + aes(alpha = factor({{ alpha_by }})) +
-        scale_alpha_manual(values = c(1.0, 0.4)) +
-        labs(alpha = param_labels[deparse(substitute(alpha_by))])
-  }
-
-  p <- p +
-    geom_line(size = 1) +
-    facet_wrap(
-      vars(variable),
-      scales = "free",
-      labeller = as_labeller(function(v) all_labels[v], label_parsed),
-      strip.position = "left"
-    ) +
-    theme(strip.background = element_blank(), strip.placement = "outside") +
-    labs(
-      x = expression(x),
-      y = NULL,
-      title = paste("Time since inflammation: ", data[, time_inf] %>% unique),
-      colour = param_labels[deparse(substitute(colour_by))]
-    )
-  p
-}
-
-spatial_plot_subset_2 <- function(time,
-                                  params_subset,
-                                  colour_by,
-                                  linetype_by,
-                                  alpha_by) {
-  data <- read_spatial_data(params, time, res_dir_base)
-
-  spatial_plot_panel <- function(variable) {
-    p <- ggplot(
-      data[
-        j_phi_i_i_factor %in% as.numeric(params_subset$j_phi_i_i_factor) &
-          m_i_factor %in% as.numeric(params_subset$m_i_factor) &
-          t_j_phi_i_lag %in% as.numeric(params_subset$t_j_phi_i_lag) &
-          gamma %in% as.numeric(params_subset$gamma) &
-          pe %in% as.numeric(params_subset$pe)
-      ],
-      aes(
-        x = x,
-        y = {{ variable }},
-        group = rep,
-        colour = factor({{ colour_by }})
-      )
-    )
-
-    p <- p +
-      geom_line(size = 1) +
-      theme(strip.background = element_blank(), strip.placement = "outside") +
-      labs(
-        x = expression(x),
-        y = variable_labels[deparse(substitute(variable))]
-      )
-    p
-  }
-
-  plots <- list()
-
-  plots[[1]] <- spatial_plot_panel(C_b) +
-    labs(
-      title = paste(
-        "Time since inflammation: ",
-        round(data[, time_inf] %>% unique, 3)
-      ),
-      colour = param_labels[deparse(substitute(colour_by))]
-    )
-  plots[[2]] <- spatial_plot_panel(phi_C_b) +
-    labs(colour = param_labels[deparse(substitute(colour_by))])
-
-  if (!missing(linetype_by)) {
-    for (i in 1:length(plots)) {
-      plots[[i]] <- plots[[i]] + aes(linetype = factor({{ linetype_by }})) +
-        labs(linetype = param_labels[deparse(substitute(linetype_by))])
-    }
-  }
-
-  if (!missing(alpha_by)) {
-    # scale_alpha_manual only works for a parameter with at most the number of
-    # values specified here
-    for (i in 1:length(plots)) {
-      plots[[i]] <- plots[[i]] + aes(alpha = factor({{ alpha_by }})) +
-        scale_alpha_manual(values = c(1.0, 0.4)) +
-        labs(alpha = param_labels[deparse(substitute(alpha_by))])
-    }
-  }
-
-  plots
-}
-
-spatial_plot_subset_3 <- function(times,
-                                  params_subset,
-                                  colour_by,
-                                  linetype_by,
-                                  alpha_by) {
   data <- lapply(
     times, function(t_inf) read_spatial_data(params, t_inf, res_dir_base)
   )
@@ -226,27 +102,6 @@ spatial_plot_subset_3 <- function(times,
   p
 }
 
-# general
-spatial_plot_general <- function(params_subset) {
-  p <- lapply(c(0, 50, 100, 150, 250, 500), function(t_inf) {
-    spatial_plot_subset_2(
-      t_inf,
-      params_subset,
-      colour_by = pe,
-      linetype_by = gamma
-    )
-  })
-
-  p <- wrap_plots(
-    rlang::flatten(p),
-    ncol = 2,
-    guides = "collect"
-    ) +
-  plot_annotation(tag_levels = "A")
-
-  p
-}
-
 plot_times <- c(0, 5, 10, 15, 25, 50)
 
 params_subset <- list(
@@ -268,7 +123,7 @@ params_subset$t_j_phi_i_lag <- 0
 params_subset$j_phi_i_i_factor <- 2
 params_subset$m_i_factor <- 2
 
-p_spatial_2_2 <- spatial_plot_subset_3(
+p_spatial_2_2 <- spatial_plot_subset(
   plot_times,
   params_subset,
   colour_by = pe,
@@ -286,7 +141,7 @@ ggsave_with_defaults(
 params_subset$j_phi_i_i_factor <- 1000
 params_subset$m_i_factor <- 2
 
-p_spatial_1000_2 <- spatial_plot_subset_3(
+p_spatial_1000_2 <- spatial_plot_subset(
   plot_times,
   params_subset,
   colour_by = pe,
@@ -304,7 +159,7 @@ ggsave_with_defaults(
 params_subset$j_phi_i_i_factor <- 2
 params_subset$m_i_factor <- 1000
 
-p_spatial_2_1000 <- spatial_plot_subset_3(
+p_spatial_2_1000 <- spatial_plot_subset(
   plot_times,
   params_subset,
   colour_by = pe,
@@ -322,7 +177,7 @@ ggsave_with_defaults(
 params_subset$j_phi_i_i_factor <- 1000
 params_subset$m_i_factor <- 1000
 
-p_spatial_1000_1000 <- spatial_plot_subset_3(
+p_spatial_1000_1000 <- spatial_plot_subset(
   plot_times,
   params_subset,
   colour_by = pe,
@@ -350,7 +205,7 @@ params_subset$t_j_phi_i_lag <- 25
 params_subset$j_phi_i_i_factor <- 2
 params_subset$m_i_factor <- 2
 
-p_spatial_2_2 <- spatial_plot_subset_3(
+p_spatial_2_2 <- spatial_plot_subset(
   plot_times,
   params_subset,
   colour_by = pe,
@@ -368,7 +223,7 @@ ggsave_with_defaults(
 params_subset$j_phi_i_i_factor <- 1000
 params_subset$m_i_factor <- 2
 
-p_spatial_1000_2 <- spatial_plot_subset_3(
+p_spatial_1000_2 <- spatial_plot_subset(
   plot_times,
   params_subset,
   colour_by = pe,
@@ -386,7 +241,7 @@ ggsave_with_defaults(
 params_subset$j_phi_i_i_factor <- 2
 params_subset$m_i_factor <- 1000
 
-p_spatial_2_1000 <- spatial_plot_subset_3(
+p_spatial_2_1000 <- spatial_plot_subset(
   plot_times,
   params_subset,
   colour_by = pe,
@@ -404,7 +259,7 @@ ggsave_with_defaults(
 params_subset$j_phi_i_i_factor <- 1000
 params_subset$m_i_factor <- 1000
 
-p_spatial_1000_1000 <- spatial_plot_subset_3(
+p_spatial_1000_1000 <- spatial_plot_subset(
   plot_times,
   params_subset,
   colour_by = pe,
