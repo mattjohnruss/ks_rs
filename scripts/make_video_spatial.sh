bec531738fd934b82e6d238fc7e37280304d289f
Matthew Russell <matthewjohnrussell@gmail.com>
2024-07-31 11:46:26 +0100

    Fix video framerate

--- scripts/make_video_spatial.sh
+++ scripts/make_video_spatial.sh
@@ -1,20 +1,20 @@
 ffmpeg \
+    -framerate 10 \
     -pattern_type glob \
     -i 'spatial_t=*_colour=m_i_factor.png' \
     -vf "drawtext=fontfile=Arial.ttf: text='%{frame_num}': start_number=0: x=2*lh: y=2*lh: fontcolor=black: fontsize=40: box=1: boxcolor=white: boxborderw=5" \
-    -r 10 \
     -y m_i_factor.mp4
 
 ffmpeg \
+    -framerate 10 \
     -pattern_type glob \
     -i 'spatial_t=*_colour=j_phi_i_i_factor.png' \
     -vf "drawtext=fontfile=Arial.ttf: text='%{frame_num}': start_number=0: x=2*lh: y=2*lh: fontcolor=black: fontsize=40: box=1: boxcolor=white: boxborderw=5" \
-    -r 10 \
     -y j_phi_i_i_factor.mp4
 
 ffmpeg \
+    -framerate 10 \
     -pattern_type glob \
     -i 'spatial_t=*_colour=t_j_phi_i_lag.png' \
     -vf "drawtext=fontfile=Arial.ttf: text='%{frame_num}': start_number=0: x=2*lh: y=2*lh: fontcolor=black: fontsize=40: box=1: boxcolor=white: boxborderw=5" \
-    -r 10 \
     -y t_j_phi_i_lag.mp4
